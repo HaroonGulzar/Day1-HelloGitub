5acf5fff79694421e2b31e32e3b572e381e17f6b
Haroon_programmer <haroongulzar226@gmail.com>
2026-08-25 16:00:50 +0500

    Day 1 - Hello Github Project

---
+++ istbasicproj.py
@@ -0,0 +1,9 @@
+import datetime
+
+def main():
+    today = datetime.date.today()
+    print("Hello GitHub! 🚀")
+    print(f"Starting my 100 Days Challenge on: {today}")
+
+if __name__ == "__main__":
+    main()
